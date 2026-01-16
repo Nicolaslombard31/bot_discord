d6ea668d0e1e90b8c1a9941e67a1b7e753d26eae
Mortarion31 <152505004+Mortarion31@users.noreply.github.com>
2026-01-16 14:51:22 +0100

    mise en place de la restriction

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
+  PermissionOverwrites,
+  PermissionFlagsBits,
+  MessageFlags,
+  Partials,
 } = require("discord.js");
 const fs = require("fs");
 const path = "./refus.json";
@@ -29,8 +33,15 @@ const bot = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.DirectMessages,
+    1 << 21,
   ],
+  partials: [Partials.Channel, Partials.Message],
 });
+
+const invites = new Map();
 
 bot.on("ready", async () => {
   console.log(`✅ Bot EPSI Bordeaux en ligne : ${bot.user.tag}`);
@@ -68,8 +79,13 @@ bot.on("ready", async () => {
             value: "Utilisez les salons appropriés et soignez votre langage.",
           },
           {
-            name: "Sanctions",
-            value: "Non-respect des règles peut entraîner des avertissements ou bannissements.",
+            name: "📚 EPSI",
+            value: "Respectez les règles de l'EPSI.",
+          },
+          {
+            name: "⚠️ Sanctions",
+            value:
+              "Non-respect des règles peut entraîner des avertissements ou bannissements de la part des modérateurs.",
           }
         )
         .setFooter({
@@ -109,7 +125,7 @@ bot.on("ready", async () => {
     if (oldMessages.size > 0)
       await channelRE.bulkDelete(oldMessages).catch(() => null);
     const sentMessageRE = await channelRE.send(
-      "Bienvenue dans le salon de Recherche d'Entreprise ! veuillers choisir votre promotion en réagissant à ce message :\n📱 : B3 CDA, \n⚙️ : B3 SRB, \n🧠 : M1 IA, \n🛡️ : M1 cyber, \n👨‍💻 : M1 Dev, \n🏗️ : M1 infra"
+      "Bienvenue dans le salon de Recherche d'Entreprise ! veuillers choisir votre promotion en réagissant à ce message :\n📱 : B3 CDA, \n⚙️ : B3 ASRBD, \n🧠 : M1 IA, \n🛡️ : M1 cyber, \n👨‍💻 : M1 Dev, \n🏗️ : M1 infra"
     );
     await sentMessageRE.react("📱");
     await sentMessageRE.react("⚙️");
@@ -118,6 +134,13 @@ bot.on("ready", async () => {
     await sentMessageRE.react("👨‍💻");
     await sentMessageRE.react("🏗️");
   }
+  bot.guilds.cache.forEach(async (guild) => {
+    const firstInvites = await guild.invites.fetch();
+    invites.set(
+      guild.id,
+      new Map(firstInvites.map((invite) => [invite.code, invite.uses]))
+    );
+  });
 });
 
 bot.on("messageReactionAdd", async (reaction, user) => {
@@ -292,9 +315,11 @@ bot.on("messageReactionAdd", async (reaction, user) => {
       .fetch(studentId)
       .catch(() => null);
     if (student) {
-      if (reaction.message.guild.roles.cache.find((r) => r.name === "B3_SRB")) {
+      if (
+        reaction.message.guild.roles.cache.find((r) => r.name === "B3_ASRBD")
+      ) {
         const role = reaction.message.guild.roles.cache.find(
-          (r) => r.name === "B3_SRB"
+          (r) => r.name === "B3_ASRBD"
         );
         await student.roles.add(role.id);
       }
@@ -407,50 +432,114 @@ bot.on("messageReactionRemove", async (reaction, user) => {
 bot.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
+  if (!message.guild) {
+    console.log(`💬 DM reçu de ${message.author.tag} : ${message.content}`);
+
+    const adminChannel = bot.channels.cache.get(process.env.ID_SALON_QUESTIONS);
+
+    if (adminChannel) {
+      if (adminChannel.type === 15) {
+        await adminChannel.threads
+          .create({
+            name: `Question de ${message.author.username}`,
+            message: {
+              content: `**Utilisateur :** <@${message.author.id}>\n**Message :** ${message.content}`,
+            },
+          })
+          .catch((err) => console.error("Erreur Forum:", err));
+      } else if (adminChannel.isTextBased()) {
+        await adminChannel
+          .send(`💬 **DM reçu de ${message.author.tag}** : ${message.content}`)
+          .catch((err) => console.error("Erreur envoi Admin:", err));
+      }
+    }
+    return;
+  }
+
   const hasRole = message.member.roles.cache.some(function (r) {
     const role = r.name.split("-");
     return role[0] === "i";
   });
 
-  if (hasRole && message.content === "!create") {
-    message.reply(
-      "Commande de création lancée ! Envoie maintenant le **nom du salon** et le **nom de la catégorie** (séparés par un espace)."
-    );
+  if (message.content === "!create") {
+    if (hasRole) {
+      message.reply(
+        "Commande de création lancée ! Envoie maintenant le **nom du salon** et le **nom de la catégorie** (séparés par un espace)."
+      );
 
-    const filter = (m) => m.author.id === message.author.id;
+      const filter = (m) => m.author.id === message.author.id;
 
-    message.channel
-      .awaitMessages({ filter, max: 1, time: 30000, errors: ["time"] })
-      .then(async (collected) => {
-        const response = collected.first();
-        const args = response.content.split(" ");
-        const category = message.guild.channels.cache.find(
-          (c) => c.name.toLowerCase() === args[1].toLowerCase() && c.type === 4
-        );
-        const channelName = args[0];
-        const categoryId = category ? category.id : null;
-
-        if (!channelName || !categoryId) {
-          return message.reply(
-            "Il manque des informations. Recommence la commande !"
+      message.channel
+        .awaitMessages({ filter, max: 1, time: 30000, errors: ["time"] })
+        .then(async (collected) => {
+          const response = collected.first();
+          const args = response.content.split(" ");
+          const category = message.guild.channels.cache.find(
+            (c) =>
+              c.name.toLowerCase() === args[1].toLowerCase() && c.type === 4
           );
-        }
+          const channelName = args[0];
+          const categoryId = category ? category.id : null;
 
-        const newChannel = await message.guild.channels.create({
-          name: channelName,
-          type: 0,
-          parent: categoryId,
+          if (!channelName || !categoryId) {
+            return message.reply(
+              "Il manque des informations. Recommence la commande !"
+            );
+          }
+          const roleInter = message.member.roles.cache.find((r) =>
+            r.name.startsWith("i-")
+          );
+          if (!roleInter) {
+            return message.reply(
+              "❌ Erreur : Je n'ai pas trouvé votre rôle d'intervenant (commençant par 'i-')."
+            );
+          }
+          const rolepromo = message.guild.roles.cache.find(
+            (r) => r.name.toLowerCase() === category.name.toLowerCase()
+          );
+          if (!rolepromo) {
+            return message.reply(
+              "❌ Erreur : Je n'ai pas trouvé le rôle de la promo."
+            );
+          }
+
+          const newChannel = await message.guild.channels.create({
+            name: channelName,
+            type: 0,
+            parent: categoryId,
+            permissionOverwrites: [
+              {
+                id: message.guild.id,
+                deny: [PermissionFlagsBits.ViewChannel],
+              },
+              {
+                id: roleInter.id,
+                allow: [
+                  PermissionFlagsBits.ViewChannel,
+                  PermissionFlagsBits.SendMessages,
+                  PermissionFlagsBits.ManageMessages,
+                ],
+              },
+              {
+                id: rolepromo.id,
+                allow: [
+                  PermissionFlagsBits.ViewChannel,
+                  PermissionFlagsBits.SendMessages,
+                ],
+              },
+            ],
+          });
+
+          message.reply(`Salon **${channelName}** créé avec succès !`);
+        })
+        .catch(() => {
+          message.reply(
+            "Temps écoulé ! Tu as mis trop de temps à répondre, commande annulée."
+          );
         });
-
-        message.reply(`Salon **${channelName}** créé avec succès !`);
-      })
-      .catch(() => {
-        message.reply(
-          "Temps écoulé ! Tu as mis trop de temps à répondre, commande annulée."
-        );
-      });
-  } else {
-    message.reply("Vous n'avez pas la permission d'utiliser cette commande.");
+    } else {
+      message.reply("Vous n'avez pas la permission d'utiliser cette commande.");
+    }
   }
 });
 
@@ -459,19 +548,13 @@ bot.on("interactionCreate", async (interaction) => {
 
   if (interaction.customId.startsWith("help_request_")) {
     const studentId = interaction.customId.split("_")[2];
-    const adminChannel = bot.channels.cache.get(process.env.ID_SALON_ADMIN);
-
-    if (adminChannel) {
-      await adminChannel.send(
-        `**Demande d'aide** : L'utilisateur <@${studentId}> (ID: \`${studentId}\`) a cliqué sur le bouton d'aide après son refus.`
-      );
-
-      await interaction.reply({
-        content:
-          "✅ Votre demande d'aide a été envoyée aux modérateurs. Ils reviendront vers vous dès que possible.",
-        ephemeral: true,
-      });
-    }
+    const student = await bot.users.fetch(studentId).catch(() => null);
+    const dmChannel = await student.createDM();
+    student
+      .send({
+        content: `Veuillez décrire votre problème en détail. Un modérateur vous répondra dès que possible.`,
+      })
+      .catch(() => null);
   }
   if (interaction.customId === "accept_rules") {
     const member = interaction.member;
@@ -481,9 +564,65 @@ bot.on("interactionCreate", async (interaction) => {
       await interaction.reply({
         content:
           "✅ Merci d'avoir accepté le règlement ! Vous avez maintenant accès au reste du serveur.",
-        ephemeral: true,
+        flags: [MessageFlags.Ephemeral],
       });
     }
+  }
+});
+
+bot.on("guildMemberAdd", async (member) => {
+  const newInvites = await member.guild.invites.fetch();
+  const oldInvites = invites.get(member.guild.id);
+
+  const inviteUsed = newInvites.find(
+    (i) => i.uses > (oldInvites.get(i.code) || 0)
+  );
+
+  if (inviteUsed && inviteUsed.code === process.env.INVITE_CODE_SPECIAL) {
+    console.log(`L'élève ${member.user.tag} est arrivé via le lien spécial !`);
+
+    const role = member.guild.roles.cache.find((r) => r.name === "Membre_RE");
+    if (role) {
+      await member.roles.add(role).catch(console.error);
+    }
+  }
+
+  invites.set(
+    member.guild.id,
+    new Map(newInvites.map((invite) => [invite.code, invite.uses]))
+  );
+});
+
+bot.on("autoModerationActionExecution", async (execution) => {
+  try {
+    const member = await execution.guild.members.fetch(execution.userId).catch(() => null);
+    
+    if (!member) {
+      console.log("❌ Impossible de récupérer le membre sur le serveur.");
+      return;
+    }
+
+    const roleRestriction = execution.guild.roles.cache.find(r => r.name === "Restriction");
+    const roleMember = execution.guild.roles.cache.find(r => r.name === "Membre");
+
+    if (!roleRestriction) {
+      console.log("⚠️ Le rôle 'restriction' n'existe pas. Vérifie l'orthographe (minuscules/majuscules).");
+      return;
+    }
+
+    await member.roles.add(roleRestriction);
+    console.log(`✅ Rôle ${roleRestriction.name} ajouté à ${member.user.tag}`);
+
+    if (roleMember && member.roles.cache.has(roleMember.id)) {
+      await member.roles.remove(roleMember);
+      console.log(`✅ Rôle ${roleMember.name} retiré à ${member.user.tag}`);
+    }
+
+    await member.send(`⚠️ Ton message a été bloqué sur **${execution.guild.name}**. Tu as été restreint.`)
+    .catch(() => console.log("DMs fermés."));
+  } catch (error) {
+    console.error("❌ Erreur lors de l'exécution de l'AutoMod :");
+    console.error(error);
   }
 });
 
